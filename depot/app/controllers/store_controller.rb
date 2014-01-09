0222174eaec1de6c027e587878b2583fddce5802
Marco Tello <contact@marcotello.com>
2014-01-09 15:01:50 -0600

    Third iteration, addign the cart functionality

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
+  before_action :count_store_views, only: [:index]
+
   def index
   	@products = Product.order(:title)
+  	@views = session[:store_views]
+  end
+
+  def count_store_views
+  	if session[:store_views].nil?
+  		session[:store_views] = 1
+  	else
+  		session[:store_views] += 1
+  	end
   end
 end
